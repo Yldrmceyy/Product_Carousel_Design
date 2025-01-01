f631b7340135c07f1f7d065e4ab6e72f9df0ad16
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2025-01-01 21:36:53 +0300

    A fetch function to pull products from the API

--- project.js
+++ project.js
@@ -5,6 +5,18 @@
     self.setEvents();
   };
 
+  const fetchProducts = async () => {
+    const localProducts = localStorage.getItem('products');
+    if (localProducts) {
+        return JSON.parse(localProducts); // Local Storage'dan veri varsa onu döndür.
+    }
+
+    // API'den veri çek.
+    const response = await fetch('https://gist.githubusercontent.com/sevindi/5765c5812bbc8238a38b3cf52f233651/raw/56261d81af8561bf0a7cf692fe572f9e1e91f372/products.json');
+    const products = await response.json();
+    localStorage.setItem('products', JSON.stringify(products)); // Local Storage'a kaydet.
+    return products;
+};
   const buildHTML = () => {
     const html = `
            <div class="carousel-container">
@@ -21,7 +33,7 @@
                 `
                   )
                   .join("")}
-        </div>
+            </div>
             <button class="carousel-btn left">←</button>
             <button class="carousel-btn right">→</button>
         </div>
