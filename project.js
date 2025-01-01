cefe057b7bc8ce275243ab2538fdfb61fa078685
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2025-01-01 21:04:14 +0300

    Dynamic HTML created with buildHTML and appended to .product-detail

--- project.js
+++ project.js
@@ -5,7 +5,15 @@
         self.setEvents();
     }
 
+    const buildHTML=()=>{
+        const html=`
+            <div class="container">
+                <h1></h1>
+            </div>
+        `;
 
+        $('.product-detail').append(html);
+    }
 
 
 
